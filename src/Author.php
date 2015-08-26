fdbec51e0649373e3e0f0a968549a522c70b013b
Julio Diaz & Tyler Deem <student@epicodus.com>
2015-08-26 11:30:24 -0700

    finish pt 1 of book/author

---
+++ src/Author.php
@@ -0,0 +1,105 @@
+<?php 
+	
+	class Author
+	{
+		private $name;
+		private $id;
+		
+		function __construct($name, $id = null)
+		{
+			$this->name = $name;
+			$this->id = $id;
+		}
+		
+		function setName($new_name)
+		{
+			$this->name =name;
+		}
+		
+		function getName()
+		{
+			return $this->name;
+		}
+		
+		function setId($new_id)
+		{
+			$this->id = $new_id;
+		}
+		
+		function getId()
+		{
+			return $this->id;
+		}
+		
+		function save()
+		{
+			$GLOBALS['DB']->exec("INSERT INTO authors (name) VALUES ('{$this->getName()}');");
+			$this->setId($GLOBALS['DB']->lastInsertId());
+		}
+		
+		function update($new_name)
+		{
+			$GLOBALS['DB']->exec("UPDATE authors SET name = '{$new_name}' WHERE id = {$this->getId()};");
+			$this->setName($new_name);
+		}
+		
+		static function getAll()
+		{
+			$returned_authors = $GLOBALS['DB']->query("SELECT * FROM authors;");
+			$authors = array();
+			foreach($returned_authors as $author)
+			{
+				$name = $author['name'];
+				$this = $book['id'];
+				$new_author = new Author($name, $id);
+				array_push($authors, $new_author);
+			}
+			return $authors;
+		}
+		
+		static function deleteAll()
+		{
+			$GLOBALS['DB']->exec("DELETE FROM books;");
+		}
+		
+		function delete()
+		{
+			$GLOBALS['DB']->exec("DELETE FROM authors WHERE id = {$this->getId()};");
+			$GLOBALS['DB']->exec("DELETE FROM authors_books WHERE author_id = {$this->getId()};");
+		}
+		
+		function getBooks()
+		{
+			$books = array();
+			$results = $GLOBALS['DB']->query("SELECT books.* FROM
+											  authors JOIN authors_books ON (authors.id = authors_books.author_id)
+											  		  JOIN books ON (authors_books.book_id = books.id)
+											  WHERE authors.id = {$this->getId()};");
+			foreach($results as $book) {
+				$book_title = $book['title'];
+				$book_id = $book['id'];
+				$new_book = new Book($book_title, $book_id);
+				array_push($books, $new_book);
+			}
+			return $books;
+		}
+		
+		function addBook($new_book)
+		{
+			$GLOBALS['DB']->exec("INSERT INTO authors_books (author_id, book_id) VALUES ({$new_book->getId()}, {$this->getId()});");
+		}
+		
+		static function find($search_id)
+		{
+			$found_author = null;
+			$authors = Author::getAll();
+			foreach($authors as $author) {
+				$author_id = $author->getId();
+				if($author_id == $search_id) {
+					$found_author = $author;
+				}
+			}
+			return $found_author;
+		}
+	}
+?>
--- src/Book.php
+++ src/Book.php
@@ -33,8 +33,14 @@
 		
 		function save()
 		{
-			$GLOBALS['DB']->exec("INSERT INTO books (title) VALUES ({$this->getTitle()});");
+			$GLOBALS['DB']->exec("INSERT INTO books (title) VALUES ('{$this->getTitle()}');");
 			$this->setId($GLOBALS['DB']->lastInsertId());
+		}
+		
+		function update($new_title)
+		{
+			$GLOBALS['DB']->exec("UPDATE books SET title = '{$new_title}' WHERE id = {$this->getId()};");
+			$this->setTitle($new_title);
 		}
 		
 		static function getAll()
@@ -54,6 +60,12 @@
 		static function deleteAll()
 		{
 			$GLOBALS['DB']->exec("DELETE FROM books;");
+		}
+		
+		function delete()
+		{
+			$GLOBALS['DB']->exec("DELETE FROM books WHERE id = {$this->getId()};");
+			$GLOBALS['DB']->exec("DELETE FROM authors_books WHERE book_id = {$this->getId()};");
 		}
 		
 		function getAuthors()
@@ -82,7 +94,7 @@
 			$found_book = null;
 			$books = Book::getAll();
 			foreach($books as $book) {
-				$book_id = $student->getId();
+				$book_id = $book->getId();
 				if($book_id == $search_id) {
 					$found_book = $book;
 				}
